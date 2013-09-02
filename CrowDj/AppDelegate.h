fd0882235be5de6c56d5587b8020d53a130617ea
Antonio Grimaldo <grimi94@gmail.com>
2013-09-02 16:01:08 -0500

    Proyect Created and Upload

---
+++ CrowDj/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CrowDj
+//
+//  Created by Grimi on 9/2/13.
+//  Copyright (c) 2013 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
